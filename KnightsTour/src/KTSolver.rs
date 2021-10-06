9eb503ac78f772a49f0708c6ce93225fdb38b415
LafeWessel <46427282+LafeWessel@users.noreply.github.com>
2021-10-06 11:53:37 -0500

    Added basic functionality to Knights Tour

---
+++ KnightsTour/src/KTSolver.rs
@@ -0,0 +1,11 @@
+struct KTSolver{
+    current_num : u32,
+    board : KTBoard,
+}
+
+
+impl KTSolver{
+    fn solveBoard(row: u32, col : u32){
+
+    }
+}
--- KnightsTour/src/main.rs
+++ KnightsTour/src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+
+mod KTBoard;
+mod KTSolver;
+
 fn main() {
     println!("Hello, world!");
+
+    // read in board size
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Unable to read line");
+    let size = input.trim().parse().expect("Unable to parse size");
+
+    let mut board = KTBoard::new(size as usize);
+    // read in starting column
+    input.clear();
+    io::stdin().read_line(&mut input).expect("Unable to read line");
+    let start_column = input.trim().parse().expect("Unable to parse starting column");
+    // read in starting row
+    input.clear();
+    io::stdin().read_line(&mut input).expect("Unable to read line");
+    let start_row = input.trim().parse().expect("Unable to parse starting row");
+    // call solver
+
+    // print board
+
 }
