c3090fe506a01ad2254b2552e3f302f0f2645d5c
LafeWessel <46427282+LafeWessel@users.noreply.github.com>
2021-10-06 13:33:06 -0500

    Added more logic to board and solver

--- KnightsTour/src/kt_board.rs
+++ KnightsTour/src/kt_board.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 pub struct knights_tour_board{
     board : Vec<Vec<i32>>,
 }
@@ -8,12 +10,26 @@ impl knights_tour_board{
         return knights_tour_board{board};
     }
 
-    pub fn get_position_value(&self, row : u32, col: u32) -> i32{
-        self.board[row as usize][col as usize]
+    // Get value of position on board, return false if unable to
+    pub fn get_position_value(&self, row : usize, col: usize) -> (i32,bool){
+        if self.valid_position(row,col){
+            return (self.board[row][col],true);
+        }
+        (0,false)
     }
 
-    pub fn set_position_value(&mut self, row: u32, col : u32, val : i32){
-        self.board[row as usize][col as usize] = val;
+    // Set value of position on board, return false if unable to
+    pub fn set_position_value(&mut self, row: usize, col : usize, val : i32) -> bool{
+        if self.valid_position(row,col){
+            self.board[row][col] = val;
+            return true;
+        }
+        return false;
+    }
+
+    // determine if position is on board (i.e. not off sides)
+    pub fn valid_position(&self, row : usize, col :usize) -> bool{
+        row >= 0 && row < self.board.len() && col >= 0 && col < self.board.len()
     }
 
     pub fn print_board(&self){
--- KnightsTour/src/kt_solver.rs
+++ KnightsTour/src/kt_solver.rs
@@ -3,16 +3,18 @@ use crate::kt_board::knights_tour_board;
 pub struct knights_tour_solver<'a>{
     current_num : u32,
     board : &'a knights_tour_board,
+    moves : [[i32;2];8],
 }
 
 
 impl knights_tour_solver<'_>{
     pub fn new(board : &knights_tour_board) -> knights_tour_solver{
-        knights_tour_solver{board, current_num : 1}
+        let moves : [[i32;2];8] = [[2,-1], [2,1], [1,-2], [1,2], [-1,-2], [-1,2], [-2,-1], [-2,1]];
+        knights_tour_solver{board, current_num : 1, moves}
     }
 
 
-    pub fn solve_board(row: u32, col : u32){
-
+    pub fn solve_board(&self, row: u32, col : u32){
+        // check each possible move
     }
 }
