0984480ea78a1ded6dd5acdd1ba4507a00849b32
LafeWessel <46427282+LafeWessel@users.noreply.github.com>
2021-10-15 14:59:20 -0500

    Completed Airport Simulation

--- AirportSimulation/src/simulation.rs
+++ AirportSimulation/src/simulation.rs
@@ -3,6 +3,8 @@ use std::fs::{File};
 use std::io::{BufReader, BufRead};
 use std::path::{Path};
 use rand::Rng;
+use std::borrow::Borrow;
+use std::ops::Deref;
 
 pub struct AirportSimulation {
     arrival_q : Vec<Plane>,
@@ -11,7 +13,7 @@ pub struct AirportSimulation {
     simulation_file : String,
     config : SimulationConfig,
     summary : SimulationSummary,
-    runways : Vec<bool>, // true = open, false = occupied
+    runways : Vec<u32>, // 0 = open, >0 = occupied
 }
 
 impl AirportSimulation {
@@ -31,7 +33,7 @@ impl AirportSimulation {
                 depart_waits : Vec::new(),
                 land_waits : Vec::new(),
             },
-            runways : vec![false;runway_count],
+            runways : vec![0;runway_count],
         }
     }
 
@@ -58,6 +60,9 @@ impl AirportSimulation {
     pub fn run_simulation(&mut self){
         println!("Running simulation for {} with {} runway(s)", self.simulation_file, self.runways.len());
         while self.timestamp < self.config.simulation_time{
+            // if self.timestamp % 1000 == 0{
+            //     println!("Current timestamp: {}", self.timestamp);
+            // }
             self.tick();
             self.timestamp += 1;
         }
@@ -70,8 +75,11 @@ impl AirportSimulation {
         self.determine_crashed();
         // - which runways are free
         // - - which planes can land or depart
+        self.land_planes();
+        self.depart_planes();
         // - whether to add any planes to depart/land queues
         self.add_to_queues();
+        self.decrement_runways();
     }
 
     /// Determine if any planes have crashed, remove them from the arrival_q and document, return local count crashed
@@ -96,7 +104,6 @@ impl AirportSimulation {
         if rand::thread_rng().gen_ratio((self.config.arrival_rate * 1000.0) as u32,1000){
             self.arrival_q.push(Plane{
                 enter_q : self.timestamp,
-                exit_q : 0,
                 in_q : true
             });
             count += 1;
@@ -105,19 +112,66 @@ impl AirportSimulation {
         if rand::thread_rng().gen_ratio((self.config.departure_rate * 1000.0) as u32, 1000){
             self.depart_q.push(Plane{
                 enter_q : self.timestamp,
-                exit_q : 0,
                 in_q : true
             });
             count += 1;
         }
         count
     }
+
+    /// determine how many planes can be departed based on open runways, return count departed
+    fn depart_planes(&mut self) -> u32{
+        let mut ct : u32 = 0;
+        for r in self.runways.iter_mut(){
+            // check if runway is open
+            if *r == 0 && self.depart_q.len() > 0{
+                // remove plane from arrival queue
+                let p: Plane = self.depart_q.remove(0);
+                // save wait time
+                self.summary.depart_waits.push(self.timestamp - p.enter_q);
+                // add to cooldown
+                *r = self.config.takeoff_duration;
+                // add to summary
+                self.summary.count_departed += 1;
+                ct += 1;
+            }
+        }
+        ct
+    }
+
+    /// determine how many planes can be landed based on open runways, return count landed
+    fn land_planes(&mut self) -> u32{
+        let mut ct : u32 = 0;
+        for r in self.runways.iter_mut(){
+            // check if runway is open
+            if *r == 0 && self.arrival_q.len() > 0{
+                // remove plane from arrival queue
+                let p: Plane = self.arrival_q.remove(0);
+                // save wait time
+                self.summary.land_waits.push(self.timestamp - p.enter_q);
+                // add to cooldown
+                *r += self.config.landing_duration;
+                // add to summary
+                self.summary.count_landed += 1;
+                ct += 1;
+            }
+        }
+        ct
+    }
+
+    /// decrement the cooldown of each runway by one
+    fn decrement_runways(&mut self){
+        for r in self.runways.iter_mut(){
+            if *r > 0{
+                *r -= 1;
+            }
+        }
+    }
 }
 
 
 struct Plane {
     enter_q : u32,
-    exit_q : u32,
     in_q : bool,
 }
 
@@ -125,7 +179,6 @@ impl Plane {
     pub fn new() -> Self{
         Plane{
             enter_q: 0,
-            exit_q: 0,
             in_q: true,
         }
     }
@@ -179,10 +232,13 @@ impl SimulationSummary{
     /// Print simulation summary details
     pub fn print_summary(&mut self){
         self.calculate_summary();
+        println!("--------------------------------");
         println!("Number of planes departed: {}", self.count_departed);
         println!("Average wait time for departing planes: {}", self.ave_departure_wait);
         println!("Number of planes landed: {}", self.count_landed);
         println!("Average wait time for landing planes {}", self.ave_land_wait);
         println!("Number of planes crashed: {}", self.count_crashed);
+        println!("--------------------------------");
+
     }
 }
