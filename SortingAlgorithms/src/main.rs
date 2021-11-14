318fbd63ca43fa7a07e88e703a7011bc2e56ea96
LafeWessel <46427282+LafeWessel@users.noreply.github.com>
2021-11-14 15:32:48 -0600

    Implemented Selection, Insertion, Bubble sorts. Merge sort partially implemented.

--- SortingAlgorithms/src/main.rs
+++ SortingAlgorithms/src/main.rs
@@ -1,5 +1,9 @@
 
 use std::fs;
+use std::cmp::Ordering;
+use std::borrow::Borrow;
+use std::fmt::Debug;
+use std::time::Instant;
 
 /// Read a file and split into lines, collecting into a Vec<String>
 fn read_file(file_name : &str) -> Vec<String>{
@@ -24,6 +28,7 @@ struct RunData{
 }
 
 impl RunData{
+    /// Print out data for an algorithm run
     fn output(&self){
         println!("Algorithm:\t\tTime:\t\tCompares:\t\tSwaps\t\t");
         println!("{:?}\t\t{}\t\t{}\t\t{}",self.algorithm,self.time,self.compares,self.swaps);
@@ -38,7 +43,8 @@ enum SortingAlgorithm{
 
 impl SortingAlgorithm{
 
-    fn sort<T : Ord>(self, list : &mut Vec<T>)
+
+    fn sort<T: Ord + Clone + Debug>(self, list : &mut Vec<T>) -> RunData
     {
         match self{
             SortingAlgorithm::Selection => SortingAlgorithm::selection_sort(list),
@@ -49,21 +55,144 @@ impl SortingAlgorithm{
         }
     }
 
-    fn selection_sort<T : Ord>(list: &mut [T]){
+    fn selection_sort<T : Ord + Clone + Debug>(mut list: &mut [T]) -> RunData{
+        // repeatedly find the smallest item and swap it with the next position of the list,
+        // starting from the front
+        let start = Instant::now();
+        let mut swaps = 0;
+        let mut compares = 0;
+
+        for i in 0..list.len(){
+            let mut smallest_T  = &list[i];
+            let mut smallest_i = i;
+            for j in i+1..list.len(){
+                smallest_T = match smallest_T.cmp(&list[j]){
+                    Ordering::Greater => {
+                        smallest_i = j;
+                        &list[j]
+                    },
+                    Ordering::Equal => smallest_T,
+                    Ordering::Less => smallest_T,
+                };
+                compares += 1;
+            }
+            list.swap(i,smallest_i);
+            swaps += 1;
+        }
+        RunData{
+            algorithm: SortingAlgorithm::Selection,
+            time: start.elapsed().as_millis() as i32,
+            compares,
+            swaps
+        }
+    }
+    fn insertion_sort<T : Ord + Clone + Debug>(list: &mut [T]) -> RunData{
+        // iterate through elements, for each element, swap it backwards until in proper spot
+        let start = Instant::now();
+        let mut compares = 0;
+        let mut swaps = 0;
+
+        for i in 1..list.len(){
+            let mut curr = list[i].clone();
+            let mut j : usize = i - 1;
+
+            while curr.cmp(&list[j]) == Ordering::Less{
+                compares += 1;
+                list.swap(j,j+1);
+                curr = list[j].clone();
+                swaps += 1;
+                j = if j == 0 {0} else {j-1};
+            }
+        }
+
+        RunData{
+            algorithm: SortingAlgorithm::Insertion,
+            time: start.elapsed().as_millis() as i32,
+            compares,
+            swaps
+        }
+    }
+    fn bubble_sort<T : Ord + Clone + Debug>(list: &mut [T]) -> RunData {
+        // for a moving window of 2 elements, move along array and swap if necessary
+        // continue until array is sorted
+
+        let start = Instant::now();
+        let mut compares = 0;
+        let mut swaps = 0;
+
+        let mut sorted : bool = false;
+
+        while !sorted{
+            let mut j = 0;
+            sorted = true;
+            for i in 0..list.len()-1{
+                j = i+1;
+                if list[i].cmp(&list[j]) == Ordering::Greater{
+                    compares += 1;
+                    list.swap(i,j);
+                    swaps += 1;
+                    sorted = false;
+                }
+            }
+        }
+
+        RunData{
+            algorithm: SortingAlgorithm::Bubble,
+            time: start.elapsed().as_millis() as i32,
+            compares,
+            swaps,
+        }
+    }
+    fn merge_sort<T : Ord + Clone + Debug>(list: &mut [T]) -> RunData{
+
+        Self::merge_sort_split(list, 0, list.len());
+
+
+
+        RunData{
+            algorithm: SortingAlgorithm::Merge,
+            time: 0,
+            compares: 0,
+            swaps: 0
+        }
+    }
+
+    /// Recursive function for splitting and recombining the array
+    fn merge_sort_split<T : Ord + Clone + Debug>(list : &mut [T], begin : usize, end : usize){
+        if (begin as i32 - end as i32) <= 1{
+            return;
+        }
+        let mid = (begin-end)/2;
+
+        // Split array into individual pieces
+        Self::merge_sort_split(list, begin, mid);
+        Self::merge_sort_split(list, mid, end);
+
+        // merge arrays back together
+        let mut i  = begin;
+        let mut j = mid;
+        while j < mid{
+            if list[i].cmp(&list[j]) == Ordering::Greater{
+                list.swap(i,j);
+                j += 1;
+            }
+            else {
+                i += 1;
+            }
+        }
 
     }
-    fn insertion_sort<T : Ord>(list: &mut [T]){
 
-    }
-    fn bubble_sort<T : Ord>(list: &mut [T]){
 
+    fn quick_sort<T : Ord + Clone + Debug>(list: &mut [T]) -> RunData{
+        RunData{
+            algorithm: SortingAlgorithm::Quicksort,
+            time: 0,
+            compares: 0,
+            swaps: 0
+        }
     }
-    fn merge_sort<T : Ord>(list: &mut [T]){
 
-    }
-    fn quick_sort<T : Ord>(list: &mut [T]){
-
-    }
 }
 
 
@@ -71,10 +200,11 @@ impl SortingAlgorithm{
 mod tests{
     use crate::SortingAlgorithm;
 
+
     #[test]
     fn insertion_sort_test(){
         // short
-        let mut v = vec![1,5,3,2,4];
+        let mut v = vec![5,1,3,2,4];
         SortingAlgorithm::sort(SortingAlgorithm::Insertion,&mut v);
         assert_eq!(vec![1,2,3,4,5],v);
 
@@ -86,7 +216,7 @@ mod tests{
         // long
         let mut v = vec![1,5,3,7,9,0,8,6,2,4,10,16,14,15,19,17,18,12,11,13];
         SortingAlgorithm::sort(SortingAlgorithm::Insertion,&mut v);
-        assert_eq!(vec![1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],v);
+        assert_eq!(vec![0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],v);
 
     }
     #[test]
@@ -104,7 +234,7 @@ mod tests{
         // long
         let mut v = vec![1,5,3,7,9,0,8,6,2,4,10,16,14,15,19,17,18,12,11,13];
         SortingAlgorithm::sort(SortingAlgorithm::Selection,&mut v);
-        assert_eq!(vec![1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],v);
+        assert_eq!(vec![0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],v);
 
     }
     #[test]
@@ -122,7 +252,7 @@ mod tests{
         // long
         let mut v = vec![1,5,3,7,9,0,8,6,2,4,10,16,14,15,19,17,18,12,11,13];
         SortingAlgorithm::sort(SortingAlgorithm::Bubble,&mut v);
-        assert_eq!(vec![1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],v);
+        assert_eq!(vec![0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],v);
 
     }
     #[test]
@@ -140,7 +270,7 @@ mod tests{
         // long
         let mut v = vec![1,5,3,7,9,0,8,6,2,4,10,16,14,15,19,17,18,12,11,13];
         SortingAlgorithm::sort(SortingAlgorithm::Merge,&mut v);
-        assert_eq!(vec![1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],v);
+        assert_eq!(vec![0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],v);
 
     }
     #[test]
@@ -158,7 +288,7 @@ mod tests{
         // long
         let mut v = vec![1,5,3,7,9,0,8,6,2,4,10,16,14,15,19,17,18,12,11,13];
         SortingAlgorithm::sort(SortingAlgorithm::Quicksort,&mut v);
-        assert_eq!(vec![1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],v);
+        assert_eq!(vec![0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],v);
 
     }
 
