42848b15258534b38167bb098c79b143820a6eb9
LafeWessel <46427282+LafeWessel@users.noreply.github.com>
2021-11-26 01:47:33 -0600

    Implemented best_fit, and added code to run and print results.

--- BinPacking/src/main.rs
+++ BinPacking/src/main.rs
@@ -1,4 +1,7 @@
-use crate::packing_algorithm::{first_fit, worst_fit, worst_fit_decreasing, best_fit};
+use crate::packing_algorithm::{first_fit, worst_fit, worst_fit_decreasing, best_fit, packing_results};
+use std::fs::File;
+use std::io::BufReader;
+use std::fs;
 
 
 mod packing_algorithm{
@@ -69,25 +72,72 @@ mod packing_algorithm{
         worst_fit(&list_sort, bin_size)
     }
 
+    /// Put next item into the Bin with the minimum necessary space
     pub fn best_fit(list : &[u32], bin_size : u32) -> Vec<Bin>{
-     vec![]
+        let mut bins = vec![Bin::new(bin_size)];
+
+        for i in list{
+            // find minimum bin with space >= current item
+            match bins.iter().filter(|b| b.space >= *i).map(|b| b.space).min(){
+                None => {
+                    // need to add a new bin
+                    let mut bin = Bin::new(bin_size);
+                    bin.add_item(*i);
+                    bins.push(bin);
+                }
+                Some(min) => {
+                    // have a bin to add to
+                    let mut bin = bins.iter_mut().find(|b| b.space == min).unwrap();
+                    bin.add_item(*i);
+                }
+            }
+
+        }
+
+        bins
     }
 
     /// Return results of a packing algorithm as a String
     pub fn packing_results(list : &[Bin]) -> String{
-        let util : f32 = list.iter().map(|b| ((b.space as f32) / (b.starting_space as f32)) as f32).sum::<f32>() / list.len() as f32;
+        let util : f32 = list.iter().map(|b| ((b.starting_space -  b.space) as f32 / (b.starting_space as f32)) * 100.0 as f32).sum::<f32>() / list.len() as f32;
         format!("Bins required: {}\nAverage utilization: {}\n", list.len(), util,)
     }
 }
 
 
 fn main() {
+    let items = read_items("./order_sizes.txt");
 
+    let item_count = [10,100,1000,10000];
+    let bin_size = [1000,5000,25000];
+    for i in item_count{
+        for b in bin_size{
+            // first
+            let v = first_fit(&items[..i],b);
+            println!("First Fit (ct:{},sz:{})\n{}",i,b,packing_results(&v));
+            // worst
+            let v = worst_fit(&items[..i],b);
+            println!("Worst Fit (ct:{},sz:{})\n{}",i,b,packing_results(&v));
+            // worst decr
+            let v = worst_fit_decreasing(&items[..i],b);
+            println!("Worst Fit Decr (ct:{},sz:{})\n{}",i,b,packing_results(&v));
+            // best
+            let v = best_fit(&items[..i],b);
+            println!("Best Fit (ct:{},sz:{})\n{}",i,b,packing_results(&v));
+        }
+    }
+}
+
+fn read_items(file_path : &str) -> Vec<u32>{
+    let mut content = fs::read_to_string(file_path).unwrap();
+    let mut items = content.lines().map(|s| s.parse().unwrap()).collect::<Vec<u32>>();
+
+    items
 }
 
 #[cfg(test)]
 mod tests{
-    use crate::packing_algorithm::{Bin, first_fit, worst_fit, worst_fit_decreasing};
+    use crate::packing_algorithm::{Bin, first_fit, worst_fit, worst_fit_decreasing, best_fit};
 
     #[test]
     fn test_first_fit(){
@@ -131,6 +181,21 @@ mod tests{
         let v = vec![b1,b2,b3];
 
         let res = worst_fit_decreasing(&list,10);
+        assert_eq!(v,res);
+    }
+
+    #[test]
+    fn test_best_fit_decreasing(){
+        let list: [u32;6] = [9,2,3,5,1,5];
+        let mut b1 = Bin::new(10);
+        b1.add_item(10);
+        let mut b2 = Bin::new(10);
+        b2.add_item(10);
+        let mut b3 = Bin::new(10);
+        b3.add_item(5);
+        let v = vec![b1,b2,b3];
+
+        let res = best_fit(&list,10);
         assert_eq!(v,res);
     }
 
